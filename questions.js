981601d3e14be0249f5f79771cab783317184d4c
TIIKKETMASTER <112451827+TIIKKETMASTER@users.noreply.github.com>
2024-01-07 16:56:15 -0800

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,18 +1,16 @@
 questions =  [
-    {"id": 0, "question": "Freedom of business is the best practical way a society can prosper.", "effects": {"right": 1}},
-    {"id": 1, "question": "People's needs should be met unconditionally.", "effects": {"right": -2}},
-    {"id": 2, "question": "Those who are not self-sufficient enough will be eliminated, naturally or not.", "effects": {"right": 1}},
-    {"id": 3, "question": "If there is a free/decentralized economy, it should be regulated.", "effects": {"right": -1}},
-    {"id": 4, "question": "A government that provides for everyone is an inherently good idea.", "effects": {"right": -1}},
-    {"id": 5, "question": "As a society, we should work to fight inequality.", "effects": {"right": -1}},
-    {"id": 6, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
-    {"id": 7, "question": "In a capitalist mode of production, welfare should be distributed financially.", "effects": {"right": -1}},
-    {"id": 8, "question": "We have a duty to serve our collective.", "effects": {"right": -1}},
-    {"id": 9, "question": "Markets are the best way to allocate resources.", "effects": {"right": 1}},
-    
-    {"id": 15, "question": "Sometimes it is right that the government may spy on its citizens to combat extremists and terrorists.", "effects": {"auth": 1}},
-    {"id": 45, "question": "The state should not exist.", "effects": {"auth": -2}}
-    {"id": 45, "question": "The state, if it must exist, should be minimized to only bare functions.", "effects": {"auth": -1}}
-    {"id": 45, "question": "Family values are sacred and should be protected.", "effects": {"prog": -1}}
-     {"id": 45, "question": "Conformity is to be expected.", "effects": {"prog": -1}}
+    {"id": 0, "question": "People's needs should be met unconditionally.", "effects": {"right": -2}},
+    {"id": 0, "question": "I value freedom of business.", "effects": {"right": 1}},
+    {"id": 0, "question": "Those who are not self-sufficient enough will be eliminated, naturally or not.", "effects": {"right": 1}},
+    {"id": 0, "question": "If there is a free/decentralized economy, it should be regulated.", "effects": {"right": -1}},
+    {"id": 0, "question": "As a society, we should work to fight inequality.", "effects": {"right": -1}},
+    {"id": 0, "question": "Land should not be a commodity to be bought and sold.", "effects": {"right": -1}},
+    {"id": 0, "question": "In a capitalist mode of production, welfare should be distributed materially.", "effects": {"right": -1}},
+    {"id": 0, "question": "We have a duty to serve our collective.", "effects": {"right": -1}},
+    {"id": 0, "question": "Markets are the best way to allocate resources.", "effects": {"right": 1}},
+    {"id": 0, "question": "The state has its very origins in coercive aggression.", "effects": {"auth": -1}},
+    {"id": 0, "question": "There is no such thing as individualism.", "effects": {"auth": 1}},
+    {"id": 0, "question": "The state, if it must exist, should be minimized to only bare functions.", "effects": {"auth": -1}},
+    {"id": 0, "question": "Family values are sacred and should be protected.", "effects": {"prog": -1}},
+     {"id": 0, "question": "Conformity is a value that should be upheld.", "effects": {"prog": -1}},
 ];
