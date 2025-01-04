06efab7294b68921eb718dad23888fd28c1ecb58
Tikket <donaldytrump05@gmail.com>
2025-01-04 00:41:21 -0800

    uploading work

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 questions =  [
-    {"id": 0, "question": "Cities, towns, and large plots of land should be privately owned and be made for the interests of its owners.", "effects": {"right": 1, "auth": 1}},
+    {"id": 0, "question": "Cities, towns, and large plots of land should be privately owned and be made for the interests of its owners.", "effects": {"right": 1}},
     {"id": 1, "question": "Everyone deserves their basic needs met.", "effects": {"right": -1}},
     {"id": 2, "question": "Courts and law firms should be privatized.", "effects": {"auth": -1}},
     {"id": 3, "question": "Governmental surveillance is essential for a functioning society.", "effects": {"auth": 1}},
@@ -9,7 +9,7 @@ questions =  [
     {"id": 7, "question": "It is wasteful to have multiple brands of the same good.", "effects": {"right": -1}},
     {"id": 8, "question": "Essential goods and industries, like water, healthcare, or housing, should not be left to the market.", "effects": {"right": -1}},
     {"id": 9, "question": "Different kinds of people should ideally keep within their own kin.", "effects": {"prog": -1}},
-    {"id": 10, "question": "State bureaucracy is inefficient.", "effects": {"auth": -1}},
+    {"id": 10, "question": "The government is less efficient than private organizations in managing resources and providing services.", "effects": {"auth": -1}},
     {"id": 11, "question": "Wages should not strictly be left to the hands of employers.", "effects": {"right": -1}},
     {"id": 12, "question": "Capitalism is the best system so far.", "effects": {"right": 2}}/* 2x */, 
     {"id": 13, "question": "There is no point in conserving cultural norms.", "effects": {"prog": 2}}/* 2x */,
@@ -20,7 +20,7 @@ questions =  [
     {"id": 18, "question": "Local communities and governments are better than large, centralized states.", "effects": {"auth": -1}},
     {"id": 19, "question": "Cooperative business models are preferable to hierarchical ones.", "effects": {"right": -1}},
     {"id": 20, "question": "The welfare state should be abolished and replaced with voluntary charitable organizations.", "effects": {"right": 1}},
-    {"id": 21, "question": "Selfishness is human nature, and should be .", "effects": {"right": 1}},
+    {"id": 21, "question": "Selfishness is human nature, and should be embraced.", "effects": {"right": 1}},
     {"id": 22, "question": "We should prevent free markets on dangerous goods like drugs, weapons, and harmful materials.", "effects": {"right": -1}},
     {"id": 23, "question": "Identification techniques should be used to ensure public safety.", "effects": {"auth": 1}},
     {"id": 24, "question": "Strong, centralized governments are ideal.", "effects": {"auth": 1}},
@@ -30,5 +30,8 @@ questions =  [
     {"id": 28, "question": "Economic class systems are divisive.", "effects": {"right": 1}},
     {"id": 29, "question": "Society was better off before the industrial revolution.", "effects": {"prog": -1}},
     {"id": 30, "question": "Political systems of the past were better than they are now.", "effects": {"prog": -1}},
-    {"id": 31, "question": "Questions of law should be defined by who owns property.", "effects": {"right": 1, "auth": -1}}
+    {"id": 31, "question": "Familial and societal values are sacred and should be protected.", "effects": {"prog": -1}},
+    {"id": 32, "question": "Tradition is antithetical to reason.", "effects": {"prog": 1}},
+    {"id": 33, "question": "Competitive markets lead to innovative productive forces.", "effects": {"right": 1}},
+    {"id": 34, "question": "Questions of law should be defined by who owns property.", "effects": {"right": 1, "auth": -1}}
 ];
