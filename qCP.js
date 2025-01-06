0e9c5dc3acc119969008c2ab873f4f6368da0f8f
Tikket <donaldytrump05@gmail.com>
2025-01-05 23:09:43 -0800

    Experimental released
    version: beta 0.6.5

---
+++ qCP.js
@@ -0,0 +1,20 @@
+questions =  [
+    {"id": 0, "question": "Cities, towns, and large plots of land should be privately owned and be made for the interests of its owners.", "effects": {"right": 1,"prog":0,"auth":0}},
+    {"id": 1, "question": "Everyone deserves their basic needs met.", "effects": {"right": -1}},
+    {"id": 5, "question": "Landlords provide an essental service.", "effects": {"right": 1}},
+    {"id": 6, "question": "It is wasteful to have multiple brands of the same good.", "effects": {"right": -1}},
+    {"id": 7, "question": "Essential goods and industries, like water, healthcare, or housing, should not be left to the market.", "effects": {"right": -1}},
+    {"id": 10, "question": "Wages should not be left to markets or the hands of employers.", "effects": {"right": -1}},
+    {"id": 11, "question": "Capitalism is the best system so far.", "effects": {"right": 1.5}}/* large */, 
+    {"id": 15, "question": "Economic inequality is not as bad as people say it is.", "effects": {"right": 1}},
+    {"id": 16, "question": "The government should intervene in the economy whenever needed.", "effects": {"right": -1}},
+    {"id": 18, "question": "Cooperative and democratic workplace models are preferable to hierarchical ones.", "effects": {"right": -1}}/* thx camel for helping rewrite this one */,
+    {"id": 19, "question": "The welfare state should be abolished and replaced with voluntary charitable organizations.", "effects": {"right": 1}},
+    {"id": 20, "question": "Selfishness is human nature, and should be embraced.", "effects": {"right": 1}},
+    {"id": 21, "question": "We should prevent free markets on dangerous goods like drugs, weapons, and harmful materials.", "effects": {"right": -1}},
+    {"id": 24, "question": "Hierarchical economic systems are exploitative and should be abolished.", "effects": {"right": -1}},
+    {"id": 27, "question": "Economic class systems are divisive.", "effects": {"right": 1}},
+    {"id": 31, "question": "Competitive economies are preferable to equity-treated ones.", "effects": {"right": 1}}/* thx camel for helping rewrite this one */,
+    {"id": 34, "question": "Socialism offers a better and more just system than capitalism.", "effects": {"right": -1.5}}/* large *//* thx bobker */, 
+    {"id": 39, "question": "Questions of law should be defined by who owns property.", "effects": {"right": 1}}
+];
---
+++ qPC.js
@@ -0,0 +1,13 @@
+questions =  [
+    {"id": 8, "question": "Different kinds of people should ideally keep within their own kin.", "effects": {"prog": -1}},
+    {"id": 12, "question": "There is no point in conserving cultural norms.", "effects": {"prog": 1.5}}/* large */,
+    {"id": 28, "question": "Political systems that were widely established in past centuries are better than the ones in place now.", "effects": {"prog": -1}},
+    {"id": 29, "question": "Familial and societal values are sacred and should be protected.", "effects": {"prog": -1}},
+    {"id": 30, "question": "Tradition is antithetical to reason.", "effects": {"prog": 1}},
+    {"id": 33, "question": "Cultural and societal traditions strengthen communities and society at large", "effects": {"prog": -1.5}}/* large *//* thx bobker */,
+    {"id": 35, "question": "Cultural homogenization caused by globalization dilutes our traditions and values.", "effects": {"prog": -1}}/* thx bobker */, 
+    {"id": 36, "question": "Social reforms should prioritise inclusivity and equality to foster a more progressive society.", "effects": {"prog": 1}}/* thx bobker */, 
+    {"id": 37, "question": "We should treat everyone equally, regardless of status or characteristics.", "effects": {"prog": 1}}, 
+    {"id": 38, "question": "Cultural adaptation to new ideas and foreign values is necessary for societies to thrive in the modern world.", "effects": {"prog": 1}}/* thx bobker */, 
+    {"id": 39, "question": "Questions of law should be defined by who owns property.", "effects": {"right": 1}}
+];
